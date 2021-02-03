c0b4247eec72be0bf78f6057ec6880e5bf76db5c
Dale Moskoff <dalemoskoff@MacBook-Pro-41.local>
2021-02-03 14:46:29 -0500

    Added comment for clarity.

--- MyCount.sh
+++ MyCount.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Loop through a sequence of 20 numbers. In each iteration, print the number of the loop iteration and whether it is a single- or double-digit number.
+
 for Count in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
 if [ $Count -lt 10 ]
